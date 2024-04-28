bb5658ae4190e283bfc8b384f85f6e510b10b118
yzy613 <59520517+yzy613@users.noreply.github.com>
2024-04-28 09:53:39 +0800

    perf: code

--- internal/controller/count.go
+++ internal/controller/count.go
@@ -37,8 +37,8 @@ var hourAndMinuteRe = regexp.MustCompile(`^(\d{1,2}):(\d{1,2})$`)
 
 func Count(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	req := &countReq{}
 	body, _ := io.ReadAll(r.Body)
+	var req *countReq
 	err := json.Unmarshal(body, &req)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -117,122 +117,120 @@ func Count(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	func() {
-		switch {
-		case req.Remain == "j":
-			if cacheVal == nil {
-				respContent = "还没人说过" + service.MapAbbr(ctx, abbr) + "有多少人哦"
-				return
-			}
-			respContent = fmt.Sprintf("%v 在 %v 说%v有 %v 人",
-				data.UserId,
-				data.Time.Time.Format("2006-01-02 15:04:05"),
-				service.MapAbbr(ctx, abbr),
-				data.Count,
-			)
-		case req.Remain == "++":
-			data.Count++
-			if req.CountLimit > 0 && data.Count > req.CountLimit {
-				respContent = "真的有这么多人？！"
-				return
-			}
-			data.Time = gtime.Now()
-			err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				g.Log().Error(ctx, err)
-				return
-			}
-			respContent = fmt.Sprintf("%v人数加一！现在还有 %v 人",
-				service.MapAbbr(ctx, abbr),
-				data.Count,
-			)
-		case req.Remain == "--":
-			if data.Count <= 0 {
-				respContent = "诶？是不是已经没人排队了"
-				return
-			}
-			data.Count--
-			data.Time = gtime.Now()
-			err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				g.Log().Error(ctx, err)
-				return
-			}
-			respContent = fmt.Sprintf("%v人数减一！现在还有 %v 人",
-				service.MapAbbr(ctx, abbr),
-				data.Count,
-			)
-		case strings.HasPrefix(req.Remain, "+"):
-			num := gconv.Int(req.Remain[1:])
-			if num <= 0 {
-				respContent = "啊啊啊，我怎么看不懂你写的是什么"
-				return
-			}
-			data.Count += num
-			if req.CountLimit > 0 && data.Count > req.CountLimit {
-				respContent = "真的有这么多人？！"
-				return
-			}
-			data.Time = gtime.Now()
-			err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				g.Log().Error(ctx, err)
-				return
-			}
-			respContent = fmt.Sprintf("%v人数加 %v！现在还有 %v 人",
-				service.MapAbbr(ctx, abbr),
-				num,
-				data.Count,
-			)
-		case strings.HasPrefix(req.Remain, "-"):
-			num := gconv.Int(req.Remain[1:])
-			if num <= 0 {
-				respContent = "啊啊啊，我怎么看不懂你写的是什么"
-				return
-			}
-			if data.Count < num {
-				respContent = "人数不够减哦"
-				return
-			}
-			data.Count -= num
-			data.Time = gtime.Now()
-			err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				g.Log().Error(ctx, err)
-				return
-			}
-			respContent = fmt.Sprintf("%v人数减 %v！现在还有 %v 人",
-				service.MapAbbr(ctx, abbr),
-				num,
-				data.Count,
-			)
-		default:
-			var num int
-			num, err = strconv.Atoi(req.Remain)
-			if err != nil {
-				respContent = "啊啊啊，我怎么看不懂你写的是什么"
-				return
-			}
-			if req.CountLimit > 0 && num > req.CountLimit {
-				respContent = "真的有这么多人？！"
-				return
-			}
-			data.Count = num
-			data.Time = gtime.Now()
-			err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				g.Log().Error(ctx, err)
-				return
-			}
-			respContent = fmt.Sprintf("我知道%v有 %v 人啦！",
-				service.MapAbbr(ctx, abbr),
-				num,
-			)
+	switch {
+	case req.Remain == "j":
+		if cacheVal == nil {
+			respContent = "还没人说过" + service.MapAbbr(ctx, abbr) + "有多少人哦"
+			return
 		}
-	}()
+		respContent = fmt.Sprintf("%v 在 %v 说%v有 %v 人",
+			data.UserId,
+			data.Time.Time.Format("2006-01-02 15:04:05"),
+			service.MapAbbr(ctx, abbr),
+			data.Count,
+		)
+	case req.Remain == "++":
+		data.Count++
+		if req.CountLimit > 0 && data.Count > req.CountLimit {
+			respContent = "真的有这么多人？！"
+			return
+		}
+		data.Time = gtime.Now()
+		err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			g.Log().Error(ctx, err)
+			return
+		}
+		respContent = fmt.Sprintf("%v人数加一！现在还有 %v 人",
+			service.MapAbbr(ctx, abbr),
+			data.Count,
+		)
+	case req.Remain == "--":
+		if data.Count <= 0 {
+			respContent = "诶？是不是已经没人排队了"
+			return
+		}
+		data.Count--
+		data.Time = gtime.Now()
+		err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			g.Log().Error(ctx, err)
+			return
+		}
+		respContent = fmt.Sprintf("%v人数减一！现在还有 %v 人",
+			service.MapAbbr(ctx, abbr),
+			data.Count,
+		)
+	case strings.HasPrefix(req.Remain, "+"):
+		num := gconv.Int(req.Remain[1:])
+		if num <= 0 {
+			respContent = "啊啊啊，我怎么看不懂你写的是什么"
+			return
+		}
+		data.Count += num
+		if req.CountLimit > 0 && data.Count > req.CountLimit {
+			respContent = "真的有这么多人？！"
+			return
+		}
+		data.Time = gtime.Now()
+		err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			g.Log().Error(ctx, err)
+			return
+		}
+		respContent = fmt.Sprintf("%v人数加 %v！现在还有 %v 人",
+			service.MapAbbr(ctx, abbr),
+			num,
+			data.Count,
+		)
+	case strings.HasPrefix(req.Remain, "-"):
+		num := gconv.Int(req.Remain[1:])
+		if num <= 0 {
+			respContent = "啊啊啊，我怎么看不懂你写的是什么"
+			return
+		}
+		if data.Count < num {
+			respContent = "人数不够减哦"
+			return
+		}
+		data.Count -= num
+		data.Time = gtime.Now()
+		err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			g.Log().Error(ctx, err)
+			return
+		}
+		respContent = fmt.Sprintf("%v人数减 %v！现在还有 %v 人",
+			service.MapAbbr(ctx, abbr),
+			num,
+			data.Count,
+		)
+	default:
+		var num int
+		num, err = strconv.Atoi(req.Remain)
+		if err != nil {
+			respContent = "啊啊啊，我怎么看不懂你写的是什么"
+			return
+		}
+		if req.CountLimit > 0 && num > req.CountLimit {
+			respContent = "真的有这么多人？！"
+			return
+		}
+		data.Count = num
+		data.Time = gtime.Now()
+		err = cache.Set(ctx, cacheKey, data, data.Time.EndOfDay().Sub(data.Time))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			g.Log().Error(ctx, err)
+			return
+		}
+		respContent = fmt.Sprintf("我知道%v有 %v 人啦！",
+			service.MapAbbr(ctx, abbr),
+			num,
+		)
+	}
 }
