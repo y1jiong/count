50d54fe519418fabf6dbfb7021453aa63dce530f
yzy613 <59520517+yzy613@users.noreply.github.com>
2024-06-17 15:55:40 +0800

    feat: support message

--- internal/controller/count.go
+++ internal/controller/count.go
@@ -33,53 +33,63 @@ type countReq struct {
 
 type countCache struct {
 	Nickname string      `json:"nickname"`
+	Message  string      `json:"message,omitempty"`
 	UserId   int64       `json:"user_id"`
-	GroupId  int64       `json:"group_id"`
+	GroupId  int64       `json:"group_id,omitempty"`
 	Count    int         `json:"count"`
 	Time     *gtime.Time `json:"time"`
 }
 
 var (
-	hourAndMinuteRe    = regexp.MustCompile(`^(\d{1,2}):(\d{1,2})$`)
-	remainValidationRe = regexp.MustCompile(`^(?:j|几|\+\+|--|\+\d+|-\d+|\d+)$`)
+	hourAndMinuteRe = regexp.MustCompile(`^(\d{1,2}):(\d{1,2})$`)
+	remainExtractRe = regexp.MustCompile(`^(j|几个?|\+\+|--|[+-]\d+|\d+)(?:[，,]([\s\S]+))?$`)
 )
 
 func Count(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	body, _ := io.ReadAll(r.Body)
 	var req *countReq
-	err := json.Unmarshal(body, &req)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+	{
+		err := json.Unmarshal(body, &req)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 	}
 	var respContent string
 	defer func() {
 		if respContent == "" {
 			return
 		}
-		_, err = w.Write([]byte(respContent))
+		_, err := w.Write([]byte(respContent))
 		if err != nil {
 			g.Log().Error(ctx, err)
 			return
 		}
 	}()
 
-	req.Message = strings.TrimSpace(req.Message)
 	abbr := req.Message[0 : len(req.Message)-len(req.Remain)]
 	if req.FullName == "" {
 		req.FullName = abbr
 	}
 
 	req.Remain = strings.TrimSpace(req.Remain)
-	if !remainValidationRe.MatchString(req.Remain) {
+	if !remainExtractRe.MatchString(req.Remain) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
+	}
+	var cmd, msg string
+	{
+		matches := remainExtractRe.FindStringSubmatch(req.Remain)
+		cmd = matches[1]
+		if len(matches) > 2 {
+			msg = matches[2]
+		}
 	}
 
 	if req.OpenAt != "" && req.CloseAt != "" {
 		var openDur, closeDur time.Duration
-		openDur, err = time.ParseDuration(hourAndMinuteRe.ReplaceAllString(req.OpenAt, "${1}h${2}m"))
+		openDur, err := time.ParseDuration(hourAndMinuteRe.ReplaceAllString(req.OpenAt, "${1}h${2}m"))
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			return
@@ -107,16 +117,9 @@ func Count(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	cacheKey := "count_" + abbr
-	cacheVal, err := cache.Get(ctx, cacheKey)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		g.Log().Error(ctx, err)
-		return
-	}
-
 	var resetDur time.Duration
 	if req.ResetAt != "" {
+		var err error
 		resetDur, err = time.ParseDuration(hourAndMinuteRe.ReplaceAllString(req.ResetAt, "${1}h${2}m"))
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
@@ -124,21 +127,30 @@ func Count(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	cacheKey := "count_" + abbr
 	var data *countCache
-	if cacheVal != nil {
-		err = cacheVal.Scan(&data)
+	{
+		cacheVal, err := cache.Get(ctx, cacheKey)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			g.Log().Error(ctx, err)
 			return
 		}
-		if req.ResetAt != "" {
-			now := gtime.Now()
-			resetTime := now.StartOfDay().Add(resetDur)
-			if now.After(resetTime) && data.Time.Before(resetTime) {
-				data = nil
-				cacheVal = nil
-				_ = cache.Remove(ctx, cacheKey)
+		if cacheVal != nil {
+			err = cacheVal.Scan(&data)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				g.Log().Error(ctx, err)
+				return
+			}
+			if req.ResetAt != "" {
+				now := gtime.Now()
+				resetTime := now.StartOfDay().Add(resetDur)
+				if now.After(resetTime) && data.Time.Before(resetTime) {
+					data = nil
+					cacheVal = nil
+					_ = cache.Remove(ctx, cacheKey)
+				}
 			}
 		}
 	}
@@ -147,8 +159,8 @@ func Count(w http.ResponseWriter, r *http.Request) {
 	}
 
 	switch {
-	case req.Remain == "j", req.Remain == "几", req.Remain == "几个":
-		if cacheVal == nil {
+	case cmd == "j", cmd == "几", cmd == "几个":
+		if data.UserId == 0 {
 			respContent = fmt.Sprintf("还没人说过%v有多少人哦", req.FullName)
 			if req.Note != "" {
 				respContent += "\n" + req.Note
@@ -156,15 +168,19 @@ func Count(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		respContent = fmt.Sprintf("%v有 %v 人 (%v 分钟前)\n%v(%v) 在 %v 说的",
+		respContent = fmt.Sprintf("%v有 %v 人 (%v 分钟前)",
 			req.FullName,
 			data.Count,
 			gtime.Now().Sub(data.Time).Round(time.Minute).Minutes(),
+		)
+		if data.Message != "" {
+			respContent += "\n" + data.Message
+		}
+		respContent += fmt.Sprintf("\n%v(%v) 在 %v 说的",
 			data.Nickname,
 			data.UserId,
 			data.Time.Time.Format("2006-01-02 15:04:05"),
 		)
-
 		if req.Note != "" {
 			respContent += "\n" + req.Note
 		}
@@ -178,7 +194,7 @@ func Count(w http.ResponseWriter, r *http.Request) {
 		if expectWait > 0 {
 			respContent += fmt.Sprintf("\n预计等待 %v 分钟", expectWait)
 		}
-	case req.Remain == "++":
+	case cmd == "++":
 		data.Count++
 		if req.CountLimit > 0 && data.Count > req.CountLimit {
 			respContent = "真的有这么多人？！"
@@ -186,11 +202,12 @@ func Count(w http.ResponseWriter, r *http.Request) {
 		}
 
 		data.Nickname = req.Nickname
+		data.Message = msg
 		data.UserId = req.UserId
 		data.GroupId = req.GroupId
 		data.Time = gtime.Now()
 
-		err = cache.Set(ctx, cacheKey, data,
+		err := cache.Set(ctx, cacheKey, data,
 			data.Time.AddDate(0, 0, 1).StartOfDay().Add(resetDur).Sub(data.Time))
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -202,7 +219,7 @@ func Count(w http.ResponseWriter, r *http.Request) {
 			req.FullName,
 			data.Count,
 		)
-	case req.Remain == "--":
+	case cmd == "--":
 		if data.Count <= 0 {
 			respContent = "诶？是不是已经没人排队了"
 			return
@@ -210,11 +227,12 @@ func Count(w http.ResponseWriter, r *http.Request) {
 
 		data.Count--
 		data.Nickname = req.Nickname
+		data.Message = msg
 		data.UserId = req.UserId
 		data.GroupId = req.GroupId
 		data.Time = gtime.Now()
 
-		err = cache.Set(ctx, cacheKey, data,
+		err := cache.Set(ctx, cacheKey, data,
 			data.Time.AddDate(0, 0, 1).StartOfDay().Add(resetDur).Sub(data.Time))
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -226,8 +244,8 @@ func Count(w http.ResponseWriter, r *http.Request) {
 			req.FullName,
 			data.Count,
 		)
-	case strings.HasPrefix(req.Remain, "+"):
-		num := gconv.Int(req.Remain[1:])
+	case strings.HasPrefix(cmd, "+"):
+		num := gconv.Int(cmd[1:])
 		if num <= 0 {
 			w.WriteHeader(http.StatusBadRequest)
 			return
@@ -239,11 +257,12 @@ func Count(w http.ResponseWriter, r *http.Request) {
 		}
 
 		data.Nickname = req.Nickname
+		data.Message = msg
 		data.UserId = req.UserId
 		data.GroupId = req.GroupId
 		data.Time = gtime.Now()
 
-		err = cache.Set(ctx, cacheKey, data,
+		err := cache.Set(ctx, cacheKey, data,
 			data.Time.AddDate(0, 0, 1).StartOfDay().Add(resetDur).Sub(data.Time))
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -256,8 +275,8 @@ func Count(w http.ResponseWriter, r *http.Request) {
 			num,
 			data.Count,
 		)
-	case strings.HasPrefix(req.Remain, "-"):
-		num := gconv.Int(req.Remain[1:])
+	case strings.HasPrefix(cmd, "-"):
+		num := gconv.Int(cmd[1:])
 		if num <= 0 {
 			w.WriteHeader(http.StatusBadRequest)
 			return
@@ -269,11 +288,12 @@ func Count(w http.ResponseWriter, r *http.Request) {
 
 		data.Count -= num
 		data.Nickname = req.Nickname
+		data.Message = msg
 		data.UserId = req.UserId
 		data.GroupId = req.GroupId
 		data.Time = gtime.Now()
 
-		err = cache.Set(ctx, cacheKey, data,
+		err := cache.Set(ctx, cacheKey, data,
 			data.Time.AddDate(0, 0, 1).StartOfDay().Add(resetDur).Sub(data.Time))
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -287,8 +307,7 @@ func Count(w http.ResponseWriter, r *http.Request) {
 			data.Count,
 		)
 	default:
-		var num int
-		num, err = strconv.Atoi(req.Remain)
+		num, err := strconv.Atoi(cmd)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			return
@@ -300,6 +319,7 @@ func Count(w http.ResponseWriter, r *http.Request) {
 
 		data.Count = num
 		data.Nickname = req.Nickname
+		data.Message = msg
 		data.UserId = req.UserId
 		data.GroupId = req.GroupId
 		data.Time = gtime.Now()
